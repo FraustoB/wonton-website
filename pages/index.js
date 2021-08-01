4230b1470cbade697ce129ce97b63a510442121e
FraustoB <brandon.frausto18@gmail.com>
2021-07-31 21:36:11 -0500

    Adjusted layouts for mobile, added pages to be worked on in the future

--- pages/index.js
+++ pages/index.js
@@ -21,10 +21,13 @@ export default function Home() {
         Welcome To <span>WonTonSoup</span><Image src='/favicon.ico' height={50} width={50} />
       </div>
       <div className={styles.Photos}>
-
         <span><Image src='/food1.jpg' height={350} width={500} /> Authentic</span>
         <span><Image src='/food2.jpg' height={350} width={500} /> Asian</span>
         <span><Image src='/food3.jpg' height={350} width={500} /> Cuisine</span>
+      </div>
+      <div className={styles.Hours}>
+        <h2> OPEN: MONDAY-SATURDAY</h2>
+        <h3> HOURS: 11-7</h3>
       </div>
 
 
