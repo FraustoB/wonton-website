0dde6227eabc1b7d48b3b0456f3fbf1819abadd1
FraustoB <brandon.frausto18@gmail.com>
2021-08-01 21:13:33 -0500

    added ImageButton Component and added media query for image button, adjusted home page, added image button stle sheet

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
 import Head from 'next/head';
 import Image from 'next/image';
 import styles from '../styles/Home.module.scss';
+import ImageButton from '../comps/ImageButton';
 
 export default function Home() {
   return (
@@ -20,10 +21,13 @@ export default function Home() {
       <div className={styles.Intro}>
         Welcome To <span>WonTonSoup</span><Image src='/favicon.ico' height={50} width={50} />
       </div>
-      <div className={styles.Photos}>
+      {/* <div className={styles.Photos}>
         <span><Image src='/food1.jpg' height={350} width={500} /> Authentic</span>
         <span><Image src='/food2.jpg' height={350} width={500} /> Asian</span>
         <span><Image src='/food3.jpg' height={350} width={500} /> Cuisine</span>
+      </div> */}
+      <div>
+        <ImageButton image='food2.jpg' item='Drinks' />
       </div>
       <div className={styles.Hours}>
         <h2> OPEN: MONDAY-SATURDAY</h2>
